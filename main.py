16eb239d3989d3f86c39d0dec38101579e1a1dbc
Kuznetsov Ilya <kunetsoff-ilya@yandex.ru>
2022-12-09 18:03:14 +0300

    help command was added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from aiogram import Bot, types, executor
 from aiogram.dispatcher import Dispatcher
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from commands_ import commands
+from lists import texts_of_messages
 from TOKEN_FILE import TOKEN
 
 
@@ -16,5 +16,11 @@ async def send_welcome(message: types.Message):
     await message.reply('''Hi! I'm Bot.!\nTo see my commands text "/help".''')
 
 
+@dp.message_handler(commands=['help'])
+async def send_list_of_commands(message: types.Message):
+    message_text = texts_of_messages.get('commands')
+    await message.reply(message_text)
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+
