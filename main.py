e9b959c5d1381d9f6884c136fec96d563dca7873
Kuznetsov Ilya <kunetsoff-ilya@yandex.ru>
2022-12-09 17:50:35 +0300

    Mistake with answer was corrected

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=commands)
+@dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.reply('''Hi! I'm Bot.!\nTo see my commands text "/help"''')
+    await message.reply('''Hi! I'm Bot.!\nTo see my commands text "/help".''')
 
 
 if __name__ == '__main__':
