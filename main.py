d5c4ca51d9e32eba03f3160fd0bfe5144e8d654b
Kuznetsov Ilya <kunetsoff-ilya@yandex.ru>
2022-12-09 17:47:42 +0300

    First message!

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 # encoding=utf-8
 
-from TOKEN_FILE import Token
+from aiogram import Bot, types, executor
+from aiogram.dispatcher import Dispatcher
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from commands_ import commands
+from TOKEN_FILE import TOKEN
 
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=commands)
+async def send_welcome(message: types.Message):
+    await message.reply('''Hi! I'm Bot.!\nTo see my commands text "/help"''')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
